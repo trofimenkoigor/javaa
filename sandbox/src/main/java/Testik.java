6d09b9b94a3a24a00301cf5f2878bd3573bbec1b
Igor <igor@Tonys-MacBook-Pro.local>
2018-11-16 23:39:06 +0200

    Second commit

--- sandbox/src/main/java/Testik.java
+++ sandbox/src/main/java/Testik.java
@@ -1,6 +1,9 @@
 public class Testik {
 	
 	public static void main(String[] args) {
-    System.out.println("Fack you");
+
+	    System.out.println("Fack you");
+
+        System.out.println("I'SECOND COMMIT");
    }
 }
